e31c22807cb23cd1ac01a097a2e4c1bf7d1ef0db
pc <pc@RISHABHPRAKASHJAIN>
2021-06-08 21:55:18 +0530

    Mysql Connection Demo

---
+++ MysqlConnectionDemo/src/main/java/com/mysql/connection/MysqlConnectionDemoApplication.java
@@ -0,0 +1,13 @@
+package com.mysql.connection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlConnectionDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysqlConnectionDemoApplication.class, args);
+	}
+
+}
---
+++ MysqlConnectionDemo/src/test/java/com/mysql/connection/MysqlConnectionDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysql.connection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysqlConnectionDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
